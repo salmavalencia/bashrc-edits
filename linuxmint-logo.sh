da16e8121733e181cc4f839da59d094b48ea6e34
salmavalencia <48778311+salmavalencia@users.noreply.github.com>
2020-04-25 22:45:02 -0400

    Create linuxmint-logo.sh

---
+++ linuxmint-logo.sh
@@ -0,0 +1,21 @@
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+echo -e "${BLUE}MMMMMMMMMMMMMMMMMMMMMMMMMmds+.${NC}"
+echo -e "${BLUE}MMm----::-://////////////oymNMd+"${NC}
+echo -e "${BLUE}MMd${NC}      /++                ${BLUE}-sNMd:${NC}"
+echo -e "${BLUE}MMNso/${NC}   dMM     .::-. .-::.  ${BLUE}.hMN:${NC}"
+echo -e "${BLUE}ddddMMh${NC}  dMM   :hNMNMNhNMNMNh:  ${BLUE}NMm${NC}"
+echo -e "    ${BLUE}NMm${NC}  dMM  .NMN/-+MMM+-/NMN  ${BLUE}dMM${NC}"
+echo -e "    ${BLUE}NMm${NC}  dMM  -MMm   MMM   dMM. ${BLUE}dMM${NC}"
+echo -e "    ${BLUE}NMm${NC}  dMM  -MMm   MMM   dMM. ${BLUE}dMM${NC}"
+echo -e "    ${BLUE}NMm${NC}  dMM  .mmd   mmm   yMM. ${BLUE}dMM${NC}"
+echo -e "    ${BLUE}NMm${NC}  dMM   ..    ...   ydm. ${BLUE}dMM${NC}"
+echo -e "    ${BLUE}hMM-${NC} +MMd/-------...-:sdds  ${BLUE}dMM${NC}"
+echo -e "    ${BLUE}-NMm-${NC} :hNMNNNmdddddddddy/   ${BLUE}dMM${NC}"
+echo -e "     ${BLUE}-dMNs-${NC}  -::::-------.      ${BLUE}dMM${NC}"
+echo -e "       ${BLUE}/dMNmy+/:-------------:/yMMM${NC}"
+echo -e "         ${BLUE}./ydNMMMMMMMMMMMMMMMMMMMMM${NC}"
+echo -e "            ${BLUE}.MMMMMMMMMMMMMMMMMMM${NC}"
+echo ""
+echo ""
